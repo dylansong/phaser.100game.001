63293da2da4aba7324f8955ddf952ab416ac5574
张晓松 <dylan.song@gmail.com>
2021-09-07 11:11:27 -0700

    20210907111126

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -1,6 +1,10 @@
 import Phaser from 'phaser';
+import Graphics = Phaser.GameObjects.Graphics;
+import Rectangle = Phaser.GameObjects.Rectangle;
+import Pointer = Phaser.Input.Pointer;
 
 export default class Demo extends Phaser.Scene {
+  player: Rectangle | undefined;
   constructor() {
     super('GameScene');
   }
@@ -12,13 +16,25 @@ export default class Demo extends Phaser.Scene {
   create() {
     const logo = this.add.image(400, 70, 'logo');
 
-    this.tweens.add({
+
+    this.player = this.add.rectangle(30,30,50,50, 0xffffff);
+    const logoTween = this.tweens.add({
       targets: logo,
       y: 350,
-      duration: 1500,
+      duration: 1000,
       ease: 'Sine.inOut',
       yoyo: true,
       repeat: -1
     });
+    console.log(logoTween);
+
+
+  }
+  update(time: number, delta: number) {
+    const pointer = this.input.activePointer;
+    if(pointer.isDown){
+      this.player!.x = pointer.x;
+
+    }
   }
 }
